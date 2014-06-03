0e6a2109c6faf8bcff2c2de0b8c6ebb4a76d1f20
Brian Drye <bdryegit@gmail.com>
2014-06-02 21:14:18 -0700

    plot2 code and png

---
+++ plot2.R
@@ -0,0 +1,22 @@
+
+starttime <- Sys.time()
+## read data
+hpcData <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", colClasses=c(rep("character", 2), rep("numeric",7)))
+
+## convert date values to date strings
+dateStrings <- as.Date(hpcData$Date, format="%d/%m/%Y")
+
+## subset two days
+hpcDataTwoDays <- hpcData[dateStrings == "2007-02-01" | dateStrings == "2007-02-02", ]
+
+## make line graph
+with(hpcDataTwoDays, plot(strptime(paste(hpcDataTwoDays$Date, hpcDataTwoDays$Time), "%d/%m/%Y %H:%M:%S"), 
+                          Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+
+## Copy my plot to a PNG file
+dev.copy(png, file = "plot2.png")
+dev.off()
+
+## check timing 
+endtime <- Sys.time()
+endtime - starttime
